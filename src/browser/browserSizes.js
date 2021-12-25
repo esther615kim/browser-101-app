17fc983e30b004ca9845a846077f6a0801ee3f97
esther615kim <75744588+esther615kim@users.noreply.github.com>
2021-12-25 21:12:37 +0900

    Add: coordinate exercise in browserSizes.js

--- src/browser/browserSizes.js
+++ src/browser/browserSizes.js
@@ -1,26 +1,47 @@
+import { NoEmitOnErrorsPlugin } from "webpack";
 import "../styles.scss";
 const wrapper = document.querySelector(".wrapper");
-const button1 = document.getElementById("button1");
+const faTimes = document.querySelector(".fa-times");
+const coordinates = document.getElementById("coordinates");
+const playGround = document.querySelector(".play-ground");
 
 // create div
-// function createDiv(element) {
-//   let newEl = document.createElement(element);
-//   console.log(element);
-// }
+function createDiv(element, parent, className, text) {
+  const newEl = document.createElement(element);
+  newEl.classList.add(className);
+  if (text) newEl.innerText = text;
+  parent.appendChild(newEl);
+  // const content = document.createTextNode(text);
+  // newEl.appendChild(text);
+  // newEl.className.add("inactive");
+  console.log(playGround);
+}
 
-// exercise 1
-function handleClickBtn1() {
-  console.log("click exercise1");
-  const newDiv = document.createElement("div");
-  newDiv.className = "exercise1";
-  wrapper.appendChild(newDiv);
-  newDiv.style.display = "block";
+function clearPlayground() {
+  if (playGround.hasChildNodes()) {
+    playGround.childNodes.forEach((child) => playGround.removeChild(child));
+    console.log("childnodes", playGround.childNodes);
+  }
+}
+
+// exercise: coordinates
+function handleClickCoordinates() {
+  // clear
+  if (playGround.hasChildNodes()) clearPlayground();
+  // add items
+  if (!playGround.hasChildNodes()) {
+    playGround.classList.toggle("bgBlack");
+    createDiv("span", playGround, "target", "target");
+    createDiv("div", playGround, "axisX");
+    createDiv("div", playGround, "axisY");
+  }
 }
 
 // events
-button1.addEventListener("click", handleClickBtn1);
-
+coordinates.addEventListener("click", handleClickCoordinates);
+faTimes.addEventListener("click", clearPlayground);
 // window.addEventListener("resize", () => {
 //   console.log(window.screen.height);
 //   console.log(window.screen.width);
 // });
+

b03a20a220e01fde5d26b4378d89d9adfc75560d
esther615kim <vilchen@naver.com>
2021-12-28 11:07:01 +0900

    Add: exercise 1-1 scrollto/by buttons

--- README.md
+++ README.md
@@ -1,32 +1,47 @@
 ## browser-101
 Created with CodeSandbox
 ### Overview
-`Dec.21.2021-Dec.27.2021(expected)`
-- App to showcase what I've learned about Web APIs, DOM, browser events and event loop concents and exercises
+`Dec.21.2021-Dec.31.2021(expected)`
+- App to showcase what I've learned about Web APIs, DOM, browser events and event loop concents
 
 ### Stack used
--vanila javascript
--SCSS
+- vanila javascript
+- SCSS
 
 ### Screenshot
 <img src="https://user-images.githubusercontent.com/75744588/147405804-5a9b59b8-9b2a-42c4-8d26-3a14766fee91.png" width="550"> 
 
+<img src="https://images.velog.io/images/ek615/post/c1d6e043-c7f2-47e8-9c62-f75556c8dcbd/image.png" width="550"> 
+
+
+
 ### Changelog
 `Types of changes`
-`Added` for new features.
-`Changed` for changes in existing functionality.
-`Removed` for now removed features.
-`Fixed` for any bug fixes.
-`Security` in case of vulnerabilities.
-`Setting & Install` for anything installed or changed in settings.
+
+- `Added` for new features.
+- `Changed` for changes in existing functionality.
+- `Removed` for now removed features.
+- `Fixed` for any bug fixes.
+- `Security` in case of vulnerabilities.
+- `Setting & Install` for anything installed or changed in settings.
+
+[0.0.2] 2021.12.26
+
+`Changed` 
+- webAPIs.js to add exercise 1-1 scrollbuttons and 1-2 coordinates
+
+`Changed`
+- styles.scss to style dynamic web components
+
 
 [0.0.2] 2021.12.26
 
 `Added` 
-- webAPI.js
+- webAPIs.js
+
 `Changed`
 - styles.scss
-- 
+
 [0.0.1] 2021.12.21
 
 `Added`
@@ -34,6 +49,10 @@ Created with CodeSandbox
 - styles.scss
 - index.html
 - README.md
-### Lessons learned
-to-be-updated
+
+### What I've Learned & Lesson's learned
+### `Web APIs`
+https://developer.mozilla.org/en-US/docs/Web/API/Element/scrollIntoView
+`MouseEvent.clientX & clientY`
+https://developer.mozilla.org/en-US/docs/Web/API/MouseEvent/clientX
 
--- src/browser/webAPIs.js
+++ src/browser/webAPIs.js
@@ -1,30 +1,67 @@
 // import { NoEmitOnErrorsPlugin } from "webpack";
 import "../styles.scss";
-const clear = document.querySelector(".fa-times");
+const clear = document.querySelector("#clear");
+const scrollBtn = document.getElementById("scroll");
 const coordinates = document.getElementById("coordinates");
 const playGround = document.querySelector(".play-ground");
 
-// create div
+// create-div factory func
 function createDiv(element, parent, className, text) {
   const newEl = document.createElement(element);
   newEl.classList.add(className);
   if (text) newEl.innerText = text;
   parent.appendChild(newEl);
-  // const content = document.createTextNode(text);
-  // newEl.appendChild(text);
-  // newEl.className.add("inactive");
-  console.log(playGround);
 }
 
 function clearPlayground() {
-  // const nodes = playGround.hasChildNodes;
+  playGround.classList.remove("bgBlack");
+  // fix it to make it work all-together!
   if (playGround.hasChildNodes()) {
     playGround.childNodes.forEach((child) => playGround.removeChild(child));
     console.log("childnodes", playGround.childNodes);
   }
 }
+// exercise 1-1 scrol-to/by
+function handleCLickscroll() {
+  if (playGround.hasChildNodes()) clearPlayground();
+  // add items
+  if (!playGround.hasChildNodes()) {
+    createDiv("div", playGround, "scrollContainer");
+    const scrollContainer = document.querySelector(".scrollContainer");
 
-// exercise: coordinates
+    if (scrollContainer) {
+      // buttons
+      createDiv("div", scrollContainer, "btns");
+      const btns = document.querySelector(".btns");
+      if (btns) {
+        createDiv("button", btns, "scrollToSecond", "Scroll to No.2");
+        createDiv("button", btns, "scrollToSpecialBtn", "scroll to Special");
+      }
+      const scrollToSecond = document.querySelector(".scrollToSecond");
+      const scrollToSpecialBtn = document.querySelector(".scrollToSpecialBtn");
+
+      scrollToSecond.addEventListener("click", () => {
+        const secondBox = document.querySelector(".scrollBox:nth-child(2)");
+        secondBox.scrollIntoView();
+        // window.scrollTo({ top: 100, left: 0, behavior: "smooth" }); // not working
+      });
+
+      scrollToSpecialBtn.addEventListener("click", () => {
+        const specialBox = document.querySelector(".scrollBox:last-child");
+        specialBox.scrollIntoView();
+      });
+      // boxes
+      const boxes = Array.from(Array(5), (_, index) => index + 1);
+      boxes.forEach((box) =>
+        createDiv("div", scrollContainer, "scrollBox", box)
+      );
+      console.log(playGround);
+      createDiv("div", scrollContainer, "scrollBox", "I'm special");
+    }
+  }
+}
+
+// exercise1-2 coordinates
 function handleClickCoordinates() {
   // clear
   if (playGround.hasChildNodes()) clearPlayground();
@@ -47,7 +84,7 @@ function handleMouseOver(e) {
   const x = e.clientX;
   const y = e.clientY;
 
-  if (axisX && axisY) {
+  if (target && axisX && axisY) {
     axisY.style.left = `${x}px`;
     axisX.style.top = `${y}px`;
     target.style.left = `${x}px`;
@@ -61,12 +98,9 @@ function handleMouseOver(e) {
 
 // events
 coordinates.addEventListener("click", handleClickCoordinates);
+scrollBtn.addEventListener("click", handleCLickscroll);
 
 playGround.addEventListener("mouseover", handleMouseOver);
 
 clear.addEventListener("click", clearPlayground);
-
-// window.addEventListener("resize", () => {
-//   console.log(window.screen.height);
-//   console.log(window.screen.width);
-// });
+// window.scrollBy(0, 150);
